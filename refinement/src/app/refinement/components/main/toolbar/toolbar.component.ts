ab17e458cfd6feee1785aa0d490523209ef4e822
moniyat <moniyatc@gmail.com>
2020-11-30 17:43:14 -0800

    added brands to toolkit

--- refinement/src/app/refinement/components/main/toolbar/toolbar.component.ts
+++ refinement/src/app/refinement/components/main/toolbar/toolbar.component.ts
@@ -29,21 +29,77 @@ export class ToolbarComponent implements OnInit {
 
   public brands = [
     {
+      label: '& Other Stories',
+      route: 'and-other-stories'
+    },
+    {
       label: 'Abercrombie & Fitch',
       route: 'abercrombie-and-fitch'
     },
     {
-      label: 'Hollister',
-      route: 'hollister'
+      label: 'American Eagle',
+      route: 'american-eagle'
+    },
+    {
+      label: 'Celine',
+      route: 'celine'
+    },
+    {
+      label: 'Everlane',
+      route: 'everlane'
+    },
+    {
+      label: 'Forever 21',
+      route: 'forever-21'
+    },
+    {
+      label: 'Free People',
+      route: 'free-people'
+    },
+    {
+      label: 'Girlfriend Collective',
+      route: 'girlfriend-collective'
     },
     {
       label: 'H&M',
       route: 'hm'
     },
     {
-      label: 'Forever 21',
-      route: 'forever-21'
+      label: 'Hollister',
+      route: 'hollister'
     },
+    {
+      label: 'Macys',
+      route: 'macys'
+    },
+    {
+      label: 'Madewell',
+      route: 'madewell'
+    },
+    {
+      label: 'Mango',
+      route: 'mango'
+    },
+    {
+      label: 'Mara Hoffman',
+      route: 'mara-hoffman'
+    },
+    {
+      label: 'Nordstrom',
+      route: 'nordstrom'
+    },
+    {
+      label: 'Reformation',
+      route: 'reformation'
+    },
+    {
+      label: 'Topshop',
+      route: 'topshop'
+    },
+    {
+      label: 'Uniqlo',
+      route: 'uniqlo'
+    }
   ];
 
   constructor(
